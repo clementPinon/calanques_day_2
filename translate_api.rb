150ad06dc6dfade2b93e5694c73f377153d5f85e
clement <clement.pinon@gmail.com>
2016-08-24 09:37:52 +0200

    tranlate_api not finalized yet

--- translate_api.rb
+++ translate_api.rb
@@ -2,64 +2,66 @@ require 'open-uri'
 require 'JSON'
 require_relative 'tokens'
 
+class Translate
 
-def check_langage(text)
-  #define the initial langage you want to translate
-  url_lang = "https://translate.yandex.net/api/v1.5/tr.json/detect?key=#{YANDEX_TOKEN}&text=#{text}"
-  data_lang = open(url_lang).read
-  data_lang_cleaned = JSON.parse(data_lang)
+  def initialize
+    #define the initial langage you want to translate
+    url_lang = "https://translate.yandex.net/api/v1.5/tr.json/detect?key=#{YANDEX_TOKEN}&text=#{text}"
+    data_lang = open(url_lang).read
+    @data_lang_cleaned = JSON.parse(data_lang)
 
-  return data_lang_cleaned['lang']
+    url_translate = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{YANDEX_TOKEN}&lang=#{origin_lang}-#{destination_lang}&text=#{text}"
+    data_translate = open(url_translate).read
+    @data_translate_cleaned = JSON.parse(data_translate)
 
-end
-
-def destination_langage(text)
-  #puts "REQUETE"
-  origin_lang = origin_lang = check_langage(text)
-  #by default you want to translate in english. If already in english then translate in French
-
-  destination_lang = 'en'
-  if origin_lang == 'en'
-    destination_lang = 'fr'
   end
 
-  return destination_lang
+  def check_langage(text)
 
-end
+    return @data_lang_cleaned['lang']
 
-def translated_text(text, dest)
-
-  origin_lang = check_langage(text)
-  destination_lang = dest
-
-  url_translate = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{YANDEX_TOKEN}&lang=#{origin_lang}-#{destination_lang}&text=#{text}"
-  data_translate = open(url_translate).read
-  data_translate_cleaned = JSON.parse(data_translate)
-  #puts data_translate_cleaned['text'].class
-  return data_translate_cleaned['text'][0].to_s
-
-end
-
-def final_lang(abbr)
-    if abbr == 'en'
-      return 'english'
-  elsif abbr == 'fr'
-      return 'french'
-  else
-      return abbr
   end
+
+  def destination_langage(text)
+    #puts "REQUETE"
+    origin_lang = check_langage(text)
+    #by default you want to translate in english. If already in english then translate in French
+
+    destination_lang = 'en'
+    if origin_lang == 'en'
+      destination_lang = 'fr'
+    end
+
+    return destination_lang
+
+  end
+
+  def translated_text(text, dest)
+
+    origin_lang = check_langage(text)
+    destination_lang = dest
+
+
+    #puts data_translate_cleaned['text'].class
+    return data_translate_cleaned['text'][0].to_s
+
+  end
+
+  def final_lang(abbr)
+      if abbr == 'en'
+        return 'english'
+    elsif abbr == 'fr'
+        return 'french'
+    else
+        return abbr
+    end
+  end
+
+  def describe(text)
+    dest = destination_langage(text)
+    final_langage = final_lang(dest)
+    translation = translated_text(text, dest)
+    puts "#{text} means #{translation} in #{final_langage}"
+  end
+
 end
-
-def describe(text)
-  dest = destination_langage(text)
-  final_langage = final_lang(dest)
-  translation = translated_text(text, dest)
-  puts "#{text} means #{translation} in #{final_langage}"
-end
-
-describe('bonjour')
-describe('hello')
-describe('pied de table')
-
-#text = 'ola'
-#puts translate_text(text)
--- word.rb
+++ word.rb
@@ -1,1 +1,9 @@
-class
+require_relative "translate_api"
+
+class Word
+
+  def initialize(text)
+    @origin_lang =
+
+  end
+end
